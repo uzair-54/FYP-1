b8974f83bf9a00425d1c82ab1372f3a3bf750ca2
Muhammad Uzair <57868675+uzair-54@users.noreply.github.com>
2021-11-29 00:33:23 +0500

    Update faceDetector.py

--- faceDetector.py
+++ faceDetector.py
@@ -1,31 +1,94 @@
 import cv2
-# from random import randrange
 import time
+ 
+Known_distance = 76.2
 
-trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+Known_width = 14.3
+
+GREEN = (0, 255, 0)
+RED = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+fonts = cv2.FONT_HERSHEY_COMPLEX
+ 
+
+face_detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+ 
+
+def Focal_Length_Finder(measured_distance, real_width, width_in_rf_image):
+ 
+
+    focal_length = (width_in_rf_image * measured_distance) / real_width
+    return focal_length
+ 
+
+def Distance_finder(Focal_Length, real_face_width, face_width_in_frame):
+ 
+    distance = (real_face_width * Focal_Length)/face_width_in_frame
+
+    return distance
+ 
+ 
+def face_data(image):
+ 
+    face_width = 0  
+ 
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+ 
+
+    faces = face_detector.detectMultiScale(gray_image, 1.3, 5)
+ 
+    for (x, y, h, w) in faces:
+ 
+        cv2.rectangle(image, (x, y), (x+w, y+h), GREEN, 2)
+ 
+        face_width = w
+ 
+    return face_width
+ 
+ref_image = cv2.imread("ref_image.png")
+ 
+ref_image_face_width = face_data(ref_image)
+
+Focal_length_found = Focal_Length_Finder(
+    Known_distance, Known_width, ref_image_face_width)
+ 
+# print(Focal_length_found)
+ 
 cap = cv2.VideoCapture(0)
 
- 
-print("No Error")
 
 pTime = time.time()
-
 while True:
-        success, img = cap.read()
+ 
+    cTime = time.time()
+    _, frame = cap.read()
+ 
 
-        cTime = time.time()
+    face_width_in_frame = face_data(frame)
+ 
+    if face_width_in_frame != 0:
+       
 
-        
-        grayscaled_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-        face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
-        
-        for x, y, w, h in face_coordinates:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (25, 25, 222),5)
-
+        Distance = Distance_finder(
+            Focal_length_found, Known_width, face_width_in_frame)
+ 
+        cv2.line(frame, (30, 30), (230, 30), RED, 32)
+        cv2.line(frame, (30, 30), (230, 30), BLACK, 28)
+ 
+        cv2.putText(
+            frame, f"Distance: {round(Distance,2)} CM", (30, 35),
+          fonts, 0.6, GREEN, 2)
+ 
         fps = 1 / (cTime - pTime)
         pTime = cTime
         print(fps,"FPS")
-        
-        cv2.imshow("Output", img)
-        cv2.waitKey(1)
+    cv2.imshow("frame", frame)
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+ 
+cap.release()
+ 
+cv2.destroyAllWindows()
