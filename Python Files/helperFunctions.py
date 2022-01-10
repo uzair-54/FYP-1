e0484da00e1f85edc01ead668fddc9e0a070c068
uzair-54 <uzair.yameen123@gmail.com>
2022-01-10 23:57:41 +0500

    removed facelib

--- Python Files/helperFunctions.py
+++ Python Files/helperFunctions.py
@@ -9,12 +9,6 @@ def fireBaseConfigSetup():
 
 firebase, auth = fireBaseConfigSetup()
 
-def getFace(x, y, w, h, img):
-    roi = img[y:y + h, x:x + w]
-    no_of_faces, faces_coors = FL.faces_locations(roi)
-    # print(faces_coors,no_of_faces)
-    return faces_coors
-
 def getFileNames():
     classFile = 'additional Files//coco.names'
     with open(classFile, 'rt') as f:
